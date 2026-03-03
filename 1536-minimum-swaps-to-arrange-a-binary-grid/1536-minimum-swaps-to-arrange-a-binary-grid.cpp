d6b4bddad676bceee42aeddf1ba65a52dd3e5b81
DarshanSivasubramaniam <s.darshan252007@gmail.com>
2026-03-03 20:46:30 +0530

    Time: 36 ms (5%), Space: 30.6 MB (13.75%) - LeetHub

--- 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.cpp
@@ -2,40 +2,99 @@ class Solution {
 public:
     int minSwaps(vector<vector<int>>& grid) {
         
-        int n = grid.size();
-        vector<int> trailing(n);
-
-        for(int i = 0; i < n; i++) {
-            int count = 0;
-            for(int j = n - 1; j >= 0; j--) {
-                if(grid[i][j] == 0)
-                    count++;
-                else
+        int n=grid.size();
+         bool car=true;
+         
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(grid[i][j]!=0)
+                {
+                    car=false;
                     break;
+                }
             }
-            trailing[i] = count;
-        }
-
-        int swaps = 0;
-
-        for(int i = 0; i < n; i++) {
             
-            int required = n - i - 1;
-            int j = i;
+        }
 
-            while(j < n && trailing[j] < required)
-                j++;
+        if(car)return 0;
 
-            if(j == n)
-                return -1;
+        vector<vector<int>>result;
+        vector<bool>temp(n);
+        temp[0]=true;
 
-            while(j > i) {
-                swap(trailing[j], trailing[j-1]);
-                swaps++;
-                j--;
+        int count=0;
+
+        for(int i=0;i<n;i++)
+        {
+            int val=0;
+            for(int j=n-1;j>=0;j--)
+            {
+                 if(grid[i][j]==0)
+                 {
+                    val++;
+                 }
+                 else
+                 {
+                     result.push_back({val,i});
+
+                    if(val>=1 && val<n)
+                    temp[val]=true;
+                     break;
+                 }
             }
         }
 
-        return swaps;
+        // for(int i=0;i<temp.size();i++)
+        // {
+        //     if(temp[i]==false)
+        //     {
+        //         return -1;
+        //     }
+        // }
+
+        int chk=n-1;
+
+         for(auto i:result)
+        {
+            cout<<i[0]<<" "<<i[1]<<endl;
+        }
+
+        for(int i=0;i<n-1;i++)
+        {
+            cout<<"bye";
+                   int a=result[0][0];
+                   
+            bool sample=false;
+            for(int tp=0;tp<result.size();tp++)
+            {
+                if(result[tp][0]>=chk)
+                {
+                    int g=result[tp][1]-i;
+                    count+=g; 
+                    result[tp][0]=a;
+                    sample=true;
+                    break;  
+                }
+                int qwer=result[tp][0];
+                result[tp][0]=a;
+                a=qwer;
+            }
+            if(!sample) return -1;
+            chk--;
+            result.erase(result.begin());
+
+        }
+        cout<<"------------";
+
+     for(auto i:result)
+        {
+            cout<<i[0]<<" "<<i[1]<<endl;
+        }
+
+       
+
+        return count;
     }
 };
