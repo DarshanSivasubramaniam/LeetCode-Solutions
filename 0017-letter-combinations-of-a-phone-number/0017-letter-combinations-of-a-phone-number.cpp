a3f9225f30dd5c1bf0e3a465e2aabeffa90e9987
DarshanSivasubramaniam <s.darshan252007@gmail.com>
2026-03-10 16:14:51 +0530

    Time: 0 ms (100%), Space: 10.6 MB (5.7%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,56 +1,28 @@
 class Solution {
 public:
+       void func(int index,string &digits,string str,unordered_map<char,string>mp,vector<string>&result)
+       {
+           if(str.size()==digits.size())
+           {
+            result.push_back(str);
+            return;
+           }
+
+           string letters=mp[digits[index]];
+
+           for(char c:letters)
+           {
+               func(index+1,digits,str+c,mp,result);
+
+           }
+       }
+
+
     vector<string> letterCombinations(string digits) {
-        unordered_map<char, string> hash = {
-                                           {'2', "abc"},{'3',"def"},{'4',"ghi"},
-                                           {'5',"jkl"},{'6',"mno"},{'7',"pqrs"},
-                                           {'8',"tuv"},{'9',"wxyz"}};
+        unordered_map<char,string>mp={{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
+        vector<string>result;
 
-
-    vector<string>result;
-
-    for(int i=0;i<digits.size();i++)
-    {
-        char ch=digits[i];
-
-        string temp;
-           for(auto&[key,val]:hash)
-           {
-            if(key==ch)
-            {
-                temp=val;
-            }
-           }
-
-        if(i==0)
-        {
-           for(auto l:temp)
-           {
-            result.push_back(string(1,l));
-           }
-        }
-        else
-        {
-            vector<string>random;
-
-            for(string &m:result)
-            {
-                string k=m;
-                for(char p:temp)
-                {
-                    string str=k+string(1,p);
-                    random.push_back(str);
-                }
-            }
-
-            result=random;
-        }
-
-        }
+        func(0,digits,"",mp,result);
         return result;
-
-    
-
-
     }
 };
