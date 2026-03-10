631f01bd82627e9a48cd4c20d46c73fdf2c2da0f
DarshanSivasubramaniam <s.darshan252007@gmail.com>
2026-03-10 10:56:01 +0530

    Time: 0 ms (100%), Space: 10.5 MB (80.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,17 +4,13 @@ public:
         
         vector<int>dp(n+1,-1);
         dp[0]=0;
-        int offset=1;
 
         for(int i=1;i<=n;i++)
         {
-            if(offset*2==i)
-            offset=i;
-
-            dp[i]=1+dp[i-offset];
+            dp[i]=dp[i>>1] + (i&1);
         }
 
         return dp;
 
     }
-};   //2methods
+};   //2methods (1)
