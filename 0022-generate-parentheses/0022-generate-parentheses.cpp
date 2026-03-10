e71fa41795152930fe7ff9b96d4015e74580b7e0
DarshanSivasubramaniam <s.darshan252007@gmail.com>
2026-03-10 15:59:23 +0530

    Time: 1 ms (85.59%), Space: 15.6 MB (63.22%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void func(int a,int b,int n,string str,vector<string>&result)
+    {
+         if(str.size()==2*n)
+         {
+            result.push_back(str);
+            return;
+         }
+
+         if(a<n)
+         {
+            func(a+1,b,n,str+"(",result);
+         }
+
+         if(b<a)
+         {
+             func(a,b+1,n,str+")",result);
+         }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string>result;
+
+        func(0,0,n,"",result);
+        return result;
+    }
+};
