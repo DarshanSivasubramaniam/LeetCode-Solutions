5f797b13c0230019a092239ab6258495be27e309
DarshanSivasubramaniam <s.darshan252007@gmail.com>
2026-04-07 19:21:55 +0530

    Time: 22 ms (69.17%), Space: 38.1 MB (48.26%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+        set<pair<int,int>>storage;
+
+        for(auto &i:obstacles)
+        {
+            storage.insert({i[0],i[1]});
+        }
+
+        int a=0,b=0;
+        int dir=0;
+        int maxiresult=INT_MIN;
+
+        vector<vector<int>>result={{0,1},{1,0},{0,-1},{-1,0}};
+
+        for(auto &obj:commands)
+        {
+            if(obj==-1)
+            {
+               dir=(dir+1)%4;
+            }
+            else if(obj==-2)
+            {
+                dir=(dir+3)%4;
+            }
+            else
+            {
+              for(int j=0;j<obj;j++)
+              {
+                 int tempa=a+result[dir][0];
+                 int tempb=b+result[dir][1];
+
+                 if(storage.count({tempa,tempb})) break;
+
+                 a=tempa;
+                 b=tempb;
+              }
+
+              maxiresult=max({maxiresult,a*a+b*b});
+            }
+        }
+        return maxiresult;
+    }
+};
